1c70106aea3fbd5be3db9f35ddbc9aaa5e37d9c4
PatriciaJoyce <joyjoy.gavina@gmail.com>
2017-04-13 21:12:59 -0400

    update index.js

--- js/index.js
+++ js/index.js
@@ -1,20 +1,7 @@
-//include express
 const express = require('express');
-
-// static file server
-// const serveStatic = require('serve-static');
-
-// body parser middleware
-// const parser = require('body-parser');
-
-//create an express application
 const app = express();
 
-//parses requests with the content type of `application/json`
-// app.use(parser.json());
 
-
-//have the application listen on a specific port
 app.listen(1133, () => {
     console.log('Example app listening on port 1133!');
 });
