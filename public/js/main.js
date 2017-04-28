b5bb9d349d3816cb57340be733f93f2a986ff0de
Ahhhz <ozz54321@gmail.com>
2017-04-28 01:02:59 -0400

    change name of html files to match css and js file

---
+++ public/js/main.js
@@ -0,0 +1,122 @@
+(function() {
+  // console.log(login);
+  const GET = (url) => {
+    return new Promise((resolve, reject) => {
+      const http = new XMLHttpRequest();
+      http.open('GET', url);
+      http.onload = () => {
+        try {
+          const jsonData = JSON.parse(http.responseText);
+          resolve(jsonData);
+        } catch (e) {
+          reject(e);
+        }
+      } // onload
+
+      http.send();
+    });
+  } //GET
+
+
+  const POST = (url, data) => {
+    return new Promise((resolve, reject) => {
+      const http = new XMLHttpRequest();
+      http.open('POST', url);
+      http.setRequestHeader('Content-Type', 'application/json')
+      http.onload = () => {
+        try {
+          const jsonData = JSON.parse(http.responseText);
+          resolve(jsonData);
+        } catch (e) {
+          reject(e);
+        }
+      } // onload
+
+      http.send(JSON.stringify(data));
+    });
+  } //POST
+
+  const PUT = (url, data) => {
+    return new Promise((resolve, reject) => {
+      const http = new XMLHttpRequest();
+      http.open('PUT', url);
+      http.setRequestHeader('Content-Type', 'application/json');
+      http.onload = () => {
+        try {
+          const jsonData = JSON.parse(http.responseText);
+          resolve(jsonData);
+        } catch (e) {
+          reject(e);
+        }
+      } // onload
+
+      http.send(JSON.stringify(data));
+    });
+  } // PUT
+
+
+  const DELETE = (url, data) => {
+    return new Promise((resolve, reject) => {
+      const http = new XMLHttpRequest();
+      http.open('DELETE', url);
+      http.setRequestHeader('Content-Type', 'application/json');
+
+      http.onload = () => {
+        try {
+          const jsonData = JSON.parse(http.responseText);
+          resolve(jsonData);
+        } catch (e) {
+          reject(e);
+        }
+      } // onload
+
+      http.send(JSON.stringify(data));
+    });
+  } // DELETE
+
+
+
+  const submitSignUpBtn = document.querySelector('.js-signup');
+  if (submitSignUpBtn !== null) {
+    submitSignUpBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const username = document.querySelector('.js-name').value;
+      const email = document.querySelector('.js-email').value;
+      const password = document.querySelector('.js-pw').value;
+      const age = document.querySelector('.js-age').value;
+
+      POST('/auth/signup', {
+        username,
+        email,
+        password,
+      }).then((data) => {
+        console.log(data)
+        if (data.success) {
+          window.location.href = '/login.html'
+        }
+      });
+    });
+  }
+
+  const loginSignUpBtn = document.querySelector('.js-login');
+  if (loginSignUpBtn !== null) {
+    loginSignUpBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const email = document.querySelector('.js-email').value;
+      const password = document.querySelector('.js-pw').value;
+
+      POST('/auth/login', {
+        username,
+        password,
+      }).then((data) => {
+        console.log(data)
+        if (data.success) {
+          window.location.href = '/login.html'
+        }
+      });
+    });
+  }
+
+})();
